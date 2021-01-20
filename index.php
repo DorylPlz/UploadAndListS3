68ce6eaa55af5f2febe8808838e3693abcfb422a
Daryl Olivares <dolivares@marsol.cl>
2021-01-20 12:54:30 -0300

    Listar Archivos del bucket

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
     require 'start.php';
+	$config = require('config.php');
+
+    $objects = $s3->getIterator('ListObjects',[
+        'Bucket' => $config['s3']['bucket']
+    ]);
 ?>
 
 <!DOCTYPE html>
@@ -10,19 +15,22 @@
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" name="Cargar Archivo" id="submit">
         </form>
-
+<hr>
         <table>
             <th>
                 <tr>
                     <th>Nombre</th>
-                    <th>Link</th>
+                    <!--<th>Link</th> -->
                 </tr>
             </th>
             <tbody>
+            <?php foreach($objects as $object): ?>
                 <tr>
-                    <td></td>
-                    <td></td>
+                    <td><?php echo $object['Key'];?></td>
+                    <!--Link de descarga
+                    <td><a href="<?php echo $s3->getObjectUrl($config['s3']['bucket'], $object['Key']); ?>" download="<?php $object['Key']; ?>">Descarga</a></td> -->
                 </tr>
+            <?php endforeach;?>
             </tbody>
         </table>
     </body>
